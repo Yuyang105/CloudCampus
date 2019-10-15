d17fb7e95bb03b563126cd647ebefd3ac0639787
Yuyang Yu <yuyang.yu105@gmail.com>
2019-10-15 10:56:52 +0800

    Create hihi.py

---
+++ hihi.py
@@ -0,0 +1,38 @@
+# Mimic potential occurence of secrets
+aws_api_key = 'yR+uWwIZp6ihFgaHV8410b2BxbRt5QiAj1EZx1qj'
+aws_api_credential = 'yR+uWwIZp6ihFgaHV8410b2BxbRt5QiAj1EZx1qj'
+api_key_github = 'eb3692aa10723d1d3fca8a55eb78cbc9318c7a07'
+github_api_key = 'eb3692aa10723d1d3fca8a55eb78cbc9318c7a07'
+TWILLIO_API_KEY = '43d6d52880b13efc74a49be9dad59e97'
+twillio_token = '43d6d52880b13efc74a49be9dad59e97'
+Outlook_auth = 'C14294D78743194D244AF48C6757A7FF1E77CC51'
+outlook_secret_token = 'C14294D78743194D244AF48C6757A7FF1E77CC51'
+app_key = '9a3b106e41776d6567e13120910f3136'
+app_password = '9a3b106e41776d6567e13120910f3136'
+account-id = 'LTAIpTcLDo2NXYRS'
+account-secret = 'sJ66WywLqnvEpq2BtETELGrae6VuWA'
+privateToken = 'tVDXjj5sJd-rDbgMebEJ'
+SUPER_SEECRET_VALUE = 'c3VwZXIgbG9uZyBzdHJpbmcgc2hvdWxkIGNhdXNlIGVub3VnaCBlbnRyb3B5'
+VERY_SECRET_TOO = 'f6CGV4aMM9zedoh3OUNbSakBymo7yplB'
+a = '-----BEGIN OPENSSH PRIVATE KEY-----' +
+    '...key ...' +
+    '-----END PRIVATE KEY-----'
+
+
+# Confusing examples
+RevisionSeriesNumber = 'eb3692aa10723d1d3fca8a55eb78cbc9318c7a07'
+CheckingPointHash = 'eb3692aa10723d1d3fca8a55eb78cbc9318c7a07'
+UUID = 'e143708f-ef1e-4723-9e9a-94463060a012'
+user_id  = 'e143708f-ef1e-4723-9e9a-94463060a012'
+ThisIsALongString = 'CanALongStringFoolTheAlgorithms01234567890'
+Monkey = 'rLVHFDxW1MCDKeZHL9y160vWiN5djXXeV6Bmckq4'
+keyboard_series_num = 'rLVHFDxW1MCDKeZHL9y160vWiN5djXXeV6Bmckq4'
+Hash_key = '0E362922CFCA53ACF8BF929887715D8785919159'
+api_key = 'your_account_secret_key'
+accessToken = 'exampleToken19047e190mJFaL'
+password = 'example_password_demo'
+sequentialAccessKey = '0123456789abcdefABCDEF'
+api = 'notHighEnoughEntropy'
+secure = 'thequickbrownfoxjumpsoverthelazydog'
+b = '-----BEGIN OPENSSH PRIVATE KEY-----' +
+    '-----END PRIVATE KEY-----'
